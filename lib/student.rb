6ae355dcc6b594499d94f0a080983ca3157372be
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 16:40:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,9 @@ class Student
   end
   
   def update
-    
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
   end
+
 
 end
