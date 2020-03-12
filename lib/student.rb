5ee40032dee1fd21324ca2a902336d2e444d7660
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-12 16:49:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,16 +47,9 @@ class Student
   end
   
   def self.new_from_db(array)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
- 
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first #first element of the array
+    id = row[0]
+    name = row[1]
+    grade = row[2]
   end
   
   def self.find_by_name(name)
